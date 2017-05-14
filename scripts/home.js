98f90b1843f12492495e4690e4cd7abba89e112c
Vyom Malhotra <vyommalhotra@hotmail.com>
2017-05-14 17:03:15 -0400

    Adding Pages

---
+++ scripts/home.js
@@ -0,0 +1,11 @@
+//Adds a class to the navbar upon scrolling to change the color
+$(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+
+    //30% of window height
+    if (scroll >= $(window).height()*0.3 ) {
+        $(".navbar").addClass("navbar-show");
+    } else {
+        $(".navbar").removeClass("navbar-show");
+    }
+});
